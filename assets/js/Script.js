f2c024d83145318d74ba5ce951f190dc0ff8beb6
f.guglielmucci <148483575+fg-44@users.noreply.github.com>
2023-11-26 23:01:25 +0100

    update3
    update3

---
+++ assets/js/Script.js
@@ -0,0 +1,10 @@
+document.addEventListener('scroll', (e) => {
+    const header = document.querySelector('header');
+         
+    if (window.scrollY > 400) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  
+  })
